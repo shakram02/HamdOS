f29dbea4124133de7faf9e175e3045389d57ff0a
shakram02 <ahmedhamdyau@gmail.com>
2019-07-17 16:30:01 +0200

    test string with special characters

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,26 @@
+#![feature(asm)]
 #![no_std]
 
 mod vga_driver;
 
 use core::panic::PanicInfo;
 use vga_driver::VgaBuffer;
-static HELLO: &str = "HelloWOWOWOWO World!";
+static HELLO: &str = "Lorem ipsum dolor sit amet,\nno\x08n purus ut euismod vestibulum,non";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga = VgaBuffer::new();
-	vga.clear_screen();
+    let mut vga = VgaBuffer::new();
+    vga.clear_screen();
+
     vga.apply_text_attr(0x3f); // Cyan background, white text
-	vga.print(HELLO.as_bytes());
-    loop {}
+    vga.print(HELLO.as_bytes());
+
+	// Go to a terminal state
+    unsafe {
+		asm!("cli");
+        asm!("hlt");
+    }
+	panic!("END");
 }
 
 /// This function is called on panic.
