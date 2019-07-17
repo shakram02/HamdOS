acab5b44fa89f03ab5e76128d454013ef77d669f
shakram02 <ahmedhamdyau@gmail.com>
2019-07-17 16:37:52 +0200

    test string with \b and \n

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod vga_driver;
 
 use core::panic::PanicInfo;
 use vga_driver::VgaBuffer;
-static HELLO: &str = "Lorem ipsum dolor sit amet,\nno\x08n purus ut euismod vestibulum,non";
+static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\n\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning architectures with a focus on learning.";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -15,12 +15,12 @@ pub extern "C" fn _start() -> ! {
     vga.apply_text_attr(0x3f); // Cyan background, white text
     vga.print(HELLO.as_bytes());
 
-	// Go to a terminal state
+    // Go to a terminal state
     unsafe {
-		asm!("cli");
+        asm!("cli");
         asm!("hlt");
     }
-	panic!("END");
+    panic!("END");
 }
 
 /// This function is called on panic.
