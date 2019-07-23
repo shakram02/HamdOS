cf07a1aef46266d9d2456b47b0356132c9d53dde
shakram02 <ahmedhamdyau@gmail.com>
2019-07-23 08:47:52 +0200

    colored text printing, color abstractions

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,24 @@
 
 mod vga_driver;
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
-use vga_driver::VgaBuffer;
-static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\n\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning architectures with a focus on learning.";
+use vga_driver::{Color, ScreenCharAttr, VgaBuffer};
+
+static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\n\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning architectures with a focus on lear@3\x08\x08ning. Wörölö";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let mut vga = VgaBuffer::new();
     vga.clear_screen();
 
-    vga.apply_text_attr(0x3f); // Cyan background, white text
+    vga.apply_text_attr(ScreenCharAttr::new(Color::White, Color::Cyan)); // Cyan background, white text
     vga.print(HELLO.as_bytes());
-
+    vga.print_attributed_text(
+        "\n bytes: &[u8] ",
+        ScreenCharAttr::new(Color::LightBlue, Color::Brown),
+    );
+    write!(vga, "Formats! {}", 4).unwrap();
     // Go to a terminal state
     unsafe {
         asm!("cli");
--- src/vga_driver.rs
+++ src/vga_driver.rs
@@ -3,10 +3,47 @@ const SCREEN_WIDTH: usize = 80;
 const SCREEN_HEIGHT: usize = 25;
 const BACKSPACE: u8 = 8;
 const LINE_FEED: u8 = 10;
+use core::fmt;
 
 pub struct VgaBuffer {
     row: usize,
     col: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct ScreenCharAttr {
+    val: u8,
+}
+
+impl ScreenCharAttr {
+    pub fn new(foreground_color: Color, background_color: Color) -> ScreenCharAttr {
+        ScreenCharAttr {
+            val: ((background_color as u8) << 4) | (foreground_color as u8),
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
 }
 
 impl VgaBuffer {
@@ -17,12 +54,16 @@ impl VgaBuffer {
         for i in 0..SCREEN_HEIGHT {
             for j in 0..SCREEN_WIDTH {
                 self.write_byte_to_buffer_at(0x00, i as usize, j as usize);
-                self.write_attribute_to_buffer_at(0x00, i as usize, j as usize);
+                self.write_attribute_to_buffer_at(
+                    ScreenCharAttr::new(Color::Black, Color::Black),
+                    i as usize,
+                    j as usize,
+                );
             }
         }
     }
 
-    pub fn apply_text_attr(&mut self, text_attr: u8) {
+    pub fn apply_text_attr(&mut self, text_attr: ScreenCharAttr) {
         for i in 0..SCREEN_HEIGHT {
             for j in 0..SCREEN_WIDTH {
                 self.write_attribute_to_buffer_at(text_attr, i, j);
@@ -32,23 +73,40 @@ impl VgaBuffer {
 
     pub fn print(&mut self, bytes: &[u8]) {
         for &byte in bytes.iter() {
-            if byte == LINE_FEED {
-                self.row += 1;
-                self.col = 0;
-            } else if byte == BACKSPACE {
-                if self.col > 0 {
-                    self.col -= 1;
-                }
-            } else {
-                self.write_byte_to_buffer(byte);
-                self.col += 1;
+            self.print_byte(byte)
+        }
+    }
+
+    pub fn print_attributed_text(&mut self, string: &str, text_attr: ScreenCharAttr) {
+        for &byte in string.as_bytes() {
+            if is_printable(byte) {
+                self.write_attribute_to_buffer(text_attr);
             }
 
-            // Move to newline when we're about to go out of the screen
-            if self.col == SCREEN_WIDTH {
-                self.row += 1;
-                self.col = 0;
+            self.print_byte(byte);
+        }
+    }
+
+    pub fn print_byte(&mut self, byte: u8) {
+        if byte == LINE_FEED {
+            self.row += 1;
+            self.col = 0;
+        } else if byte == BACKSPACE {
+            if self.col > 0 {
+                self.col -= 1;
             }
+        } else if is_printable(byte) {
+            self.write_byte_to_buffer(byte);
+            self.col += 1;
+        } else {
+            self.write_byte_to_buffer(0xfe);
+            self.col += 1;
+        }
+
+        // Move to newline when we're about to go out of the screen
+        if self.col == SCREEN_WIDTH {
+            self.row += 1;
+            self.col = 0;
         }
     }
 
@@ -63,18 +121,29 @@ impl VgaBuffer {
         }
     }
 
-    fn write_attribute_to_buffer(&self, attr: u8) {
+    fn write_attribute_to_buffer(&self, attr: ScreenCharAttr) {
         self.write_attribute_to_buffer_at(attr, self.row, self.col);
     }
 
-    fn write_attribute_to_buffer_at(&self, attr: u8, row: usize, col: usize) {
+    fn write_attribute_to_buffer_at(&self, attr: ScreenCharAttr, row: usize, col: usize) {
         let addr = buffer_address_for(row, col) + 1;
         unsafe {
-            *(addr as *mut u8) = attr;
+            *(addr as *mut u8) = attr.val;
         }
+    }
+}
+
+impl fmt::Write for VgaBuffer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.print(s.as_bytes());
+        Ok(())
     }
 }
 
 fn buffer_address_for(row: usize, col: usize) -> usize {
     VGA_BUFFER_ADDR + ((row * (2 * SCREEN_WIDTH)) + (2 * col))
+}
+
+fn is_printable(byte: u8) -> bool {
+    byte >= 0x20 && byte <= 0x7e
 }
