40825d249008278735658e1da8bee80196fa069a
Ahmed Hamdy (@shakram02) <ahmedhamdyau@gmail.com>
2019-08-01 14:10:00 +0200

    Vga mode driver (#3)
    * initial cleanup, be a lib
    
    * initial VGA driver
    
    * adjust printing using VGA mode
    
    * test string with special characters
    
    * clean up
    
    * test string with \b and \n
    
    * colored text printing, color abstractions
    
    * scrolling screen vga driver
    
    * packages and makefile
    
    * handle panics

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,32 @@
 
 mod vga_driver;
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
-use vga_driver::VgaBuffer;
-static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\n\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning architectures with a focus on learning.";
+use vga_driver::{Color, ScreenCharAttr, VGA_WRITER};
+
+static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\n\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning architectures with a focus on lear@3\x08\x08ning. Wörölö";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut vga = VgaBuffer::new();
-    vga.clear_screen();
+    VGA_WRITER.lock().clear_screen();
 
-    vga.apply_text_attr(0x3f); // Cyan background, white text
-    vga.print(HELLO.as_bytes());
+    VGA_WRITER
+        .lock()
+        .apply_text_attr(ScreenCharAttr::new(Color::White, Color::Cyan)); // Cyan background, white text
+    VGA_WRITER.lock().print(HELLO.as_bytes());
+    VGA_WRITER.lock().print_attributed_text(
+        "\n bytes: &[u8] ",
+        ScreenCharAttr::new(Color::LightBlue, Color::Brown),
+    );
+    write!(VGA_WRITER.lock(), "Formats! {}", 4).unwrap();
+    println!();
+    println!("Hello {} {}", "!", 3);
+    for i in 0..14 {
+        println!("{}", i);
+    }
 
+    panic!("Some panic message");
     // Go to a terminal state
     unsafe {
         asm!("cli");
@@ -26,5 +40,9 @@ pub extern "C" fn _start() -> ! {
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    println!("{}", _info);
+    unsafe {
+        asm!("cli");
+        asm!("hlt");
+    }
 }
