4da8c76d82ba15d2a8b326b558c94d04861c5a86
shakram02 <ahmedhamdyau@gmail.com>
2019-08-01 18:50:55 +0200

    lib and bin

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,24 @@
-#![feature(asm)]
 #![no_std]
-#![no_main]
+#![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(asm)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_driver;
+pub mod serial;
+pub mod vga_driver;
 
-use core::fmt::Write;
 use core::panic::PanicInfo;
-use vga_driver::{Color, ScreenCharAttr, VGA_WRITER};
 
-static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\n\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning architectures with a focus on lear@3\x08\x08ning. Wörölö";
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    VGA_WRITER.lock().clear_screen();
-
-    VGA_WRITER
-        .lock()
-        .apply_text_attr(ScreenCharAttr::new(Color::White, Color::Cyan)); // Cyan background, white text
-    VGA_WRITER.lock().print(HELLO.as_bytes());
-    VGA_WRITER.lock().print_attributed_text(
-        "\n bytes: &[u8] ",
-        ScreenCharAttr::new(Color::LightBlue, Color::Brown),
-    );
-    write!(VGA_WRITER.lock(), "Formats! {}", 4).unwrap();
-    println!();
-    println!("Hello {} {}", "!", 3);
-    for i in 0..14 {
-        println!("{}", i);
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
     }
-
-    // The custom test frameworks feature generates a main function that
-    // calls test_runner, but this function is ignored because we use
-    // the #[no_main] attribute and provide our own entry point.
-    #[cfg(test)]
-    test_main(); // Generated
-
-    // Go to a terminal state
-    unsafe {
-        asm!("cli");
-        asm!("hlt");
-    };
-    panic!("END");
+    exit_qemu(QemuExitCode::Success);
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-
-    unsafe {
-        asm!("hlt");
-    }
-    loop {}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
@@ -71,20 +28,9 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
+#[panic_handler]
+pub fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -101,4 +47,16 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4); // Defined in .cargo/config
         port.write(exit_code as u32);
     }
+}
+
+/// Entry point for `cargo xtest`
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    unsafe {
+        asm!("hlt");
+    }
+    loop {}
 }
