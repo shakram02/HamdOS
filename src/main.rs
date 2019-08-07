5a16e6c33ae2f82b4e0a8ef279fd5649afd5a5ba
shakram02 <ahmedhamdyau@gmail.com>
2019-08-07 20:26:58 +0200

    use batch screen clear function

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,20 @@
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
-use ham_dos::vga_driver::{Color, ScreenCharAttr, VGA_WRITER};
-#[cfg(test)]
-use ham_dos::{exit_qemu, init, serial_println, QemuExitCode};
-use ham_dos::{print, println};
 
-static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\n\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning architectures with a focus on lear@3\x08\x08ning. Wörölö";
+#[cfg(test)]
+use ham_dos::{exit_qemu, init, QemuExitCode, serial_println};
+use ham_dos::{print, println};
+use ham_dos::vga_driver::{Color, ScreenCharAttr, VGA_WRITER};
+
+static HELLO: &str = "Computer Vision has become ubiquitous in our society, with applications in\n> search\n> image understanding\n> apps\n> mapping\n> drones, and self-driving cars.\nand self-driving cars.\nCore to many of these applications are visual recognition tasks such as image classification, localization and detection. Recent developments in neural network (aka “deep learning”) approaches have greatly advanced the performance of these state-of-the-art visual recognition systems. This course is a deep dive into details of the deep learning\n\n This course is a deep dive into details of the deep learning\n architectures with a focus on lear@3\x08\x08ning. Wörölö";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     ham_dos::init();
-    VGA_WRITER.lock().clear_screen();
-
-    VGA_WRITER
-        .lock()
-        .apply_text_attr(ScreenCharAttr::new(Color::White, Color::Cyan)); // Cyan background, white text
+    VGA_WRITER.lock().clear_text_and_apply_attr(ScreenCharAttr::new(Color::White, Color::Cyan));
     VGA_WRITER.lock().print(HELLO.as_bytes());
+
     VGA_WRITER.lock().print_attributed_text(
         "\n bytes: &[u8] ",
         ScreenCharAttr::new(Color::LightBlue, Color::Brown),
@@ -38,7 +36,7 @@ pub extern "C" fn _start() -> ! {
     // calls test_runner, but this function is ignored because we use
     // the #[no_main] attribute and provide our own entry point.
     #[cfg(test)]
-    test_main(); // Generated
+        test_main(); // Generated
 
     // Go to a terminal state
     unsafe {
